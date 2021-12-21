0beb2009b92e315ffee9e2225ee2ec2d0869349d
Jared White <jared@jaredwhite.com>
2021-12-21 10:22:20 -0800

    Fix rendering issue with mugshots

--- plugins/builders/mugshot.rb
+++ plugins/builders/mugshot.rb
@@ -1,7 +1,7 @@
 class Builders::Mugshot < SiteBuilder
   def build
     if Bridgetown.env.production?
-      hook :posts, :pre_render do |post|
+      hook :posts, :post_read do |post|
         post.data.image ||= "https://mugshotbot.com/m?theme=two_up&mode=light&color=8a1024&pattern=diagonal_lines&image=eed29abf&hide_watermark=true&url=#{CGI.escape(post.absolute_url)}"
       end
     end
